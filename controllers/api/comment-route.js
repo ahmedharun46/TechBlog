c3903144e972e0a92ffac201ff138f59734dacd2
Harun <ahmedharun46@gmail.com>
2022-10-09 16:57:31 -0500

    Adjust Routes

--- controllers/api/comment-route.js
+++ controllers/api/comment-route.js
@@ -2,6 +2,33 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+
+//GET
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.findAll();
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
 //Post
 router.post('/', withAuth, async (req, res) => {
     try {
@@ -18,13 +45,34 @@ router.post('/', withAuth, async (req, res) => {
     }
   });
 
+  //Put
+  router.put('/:id', withAuth, async (req, res) => {
+    try {
+      const commentData = await Comment.update({
+        comment_text: req.body.comment_text
+      }, {
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!commentData) {
+        res.status(404).json({ message: 'No comment found with this id!' });
+        return;
+      }
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
   //Delete
   router.delete('/:id', withAuth, async (req, res) => {
     try {
       const commentData = await Comment.destroy({
         where: {
           id: req.params.id,
-          user_id: req.session.user_id,
         },
       });
   
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,6 +3,18 @@ const { Post, User, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
+//GET
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      
+    })
+  }
+})
+
+
+
+
 //Post
 router.post('/', withAuth, async (req, res) => {
     try {
@@ -20,28 +32,26 @@ router.post('/', withAuth, async (req, res) => {
   });
 
 //Put
-  router.put('/:id', withAuth, async (req, res) => {
-    try {
-      const postData = await Post.update({
-    //     title: req.body.title,
-    //     post_content: req.body.post_content
-    //   },  
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      });
-  
-      if (!postData) {
-        res.status(404).json({ message: 'No project found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(postData);
-    } catch (err) {
-      res.status(500).json(err);
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update({
+      title: req.body.title,
+      post_content: req.body.post_content
+    }, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
     }
-  });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //Delete
   router.delete('/:id', withAuth, async (req, res) => {
@@ -50,7 +60,6 @@ router.post('/', withAuth, async (req, res) => {
      
         where: {
           id: req.params.id,
-          user_id: req.session.user_id,
         },
       });
   
